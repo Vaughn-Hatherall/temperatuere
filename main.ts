101b1f33d7094a7ac207ae7277f7977ed747decc
Vaughn-Hatherall <71399950+Vaughn-Hatherall@users.noreply.github.com>
2020-09-28 13:24:46 -0400

    Final commit

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
+let Current_temperature = 0
+// Created By:Vaughn Hatherall
+// 
+// Created on: september 28
+// 
+// This program runs temperature
+// 
 basic.forever(function () {
-	
+    Current_temperature = input.temperature()
+    basic.showNumber(Current_temperature)
+    basic.pause(1000)
+    basic.clearScreen()
 })
